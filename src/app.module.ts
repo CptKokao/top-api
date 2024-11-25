c39c5f51856847915a145933682076a022f232e1
cpt.kokao <cpt.kokao@gmail.com>
2024-11-25 15:45:21 +0300

    Подключает @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { AuthModelController } from './auth.model/auth.model.controller';
+import { AuthController } from './auth/auth.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
-  controllers: [AppController, AuthModelController],
-  providers: [AppService],
+	imports: [ConfigModule.forRoot(), AuthModule, TopPageModule, ProductModule, ReviewModule],
+	controllers: [AppController, AuthController],
+	providers: [AppService],
 })
 export class AppModule {}
