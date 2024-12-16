4b2b49f8227a85752077df6183637dd4c2351ca1
cpt.kokao <cpt.kokao@gmail.com>
2024-12-16 15:59:28 +0300

    Добавляет сервис страниц

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
 import { TopPageModel, TopPageSchema } from './top-page.model';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TopPageService } from './top-page.service';
 
 @Module({
 	controllers: [TopPageController],
-	imports: [MongooseModule.forFeature([{ name: TopPageModel.name, schema: TopPageSchema }])],
+	imports: [
+		MongooseModule.forFeature([
+			{ name: TopPageModel.name, schema: TopPageSchema },
+		]),
+	],
+	providers: [TopPageService],
 })
 export class TopPageModule {}
