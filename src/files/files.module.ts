2e5515551bb1bb18ecbd0b23841be19caef06300
cpt.kokao <cpt.kokao@gmail.com>
2024-12-25 14:40:39 +0300

    Serve файлов

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 
+//localhost:3000/static/xxxx-xx-xx/image.jpg
 @Module({
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: '/static',
+		}),
+	],
 	providers: [FilesService],
 	controllers: [FilesController],
 })
