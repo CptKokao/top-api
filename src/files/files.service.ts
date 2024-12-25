f84d8cc2f43ad55887bca18bf6a9ee9beacc83e7
cpt.kokao <cpt.kokao@gmail.com>
2024-12-25 14:35:40 +0300

    Добавляет конвертацию изображений

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,12 +3,12 @@ import { format } from 'date-fns';
 import { FileElementResponse } from './dto/file-element.response';
 import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
+import * as sharp from 'sharp';
+import { MFile } from './mfile.class';
 
 @Injectable()
 export class FilesService {
-	async saveFiles(
-		files: Express.Multer.File[],
-	): Promise<FileElementResponse[]> {
+	async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
 		const dataFolder = format(new Date(), 'yyyy-MM-dd');
 		const uploadFolder = `${path}/uploads/${dataFolder}`;
 		await ensureDir(uploadFolder);
@@ -27,5 +27,9 @@ export class FilesService {
 		}
 
 		return res;
+	}
+
+	convertToWepP(file: Buffer): Promise<Buffer> {
+		return sharp(file).webp().toBuffer();
 	}
 }
